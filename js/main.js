f5e6e3bf82bfc3df97c80051bb17c5c20812d113
Vibol <nubvibol@gmail.com>
2024-12-18 09:22:24 +0700

    remove burger menu

--- js/main.js
+++ js/main.js
@@ -21,45 +21,45 @@
 
 	var offcanvasMenu = function() {
 
-		$('#page').prepend('<div id="fh5co-offcanvas" />');
-		$('#page').prepend('<a href="#" class="js-fh5co-nav-toggle fh5co-nav-toggle fh5co-nav-white"><i></i></a>');
-		var clone1 = $('.menu-1 > ul').clone();
-		$('#fh5co-offcanvas').append(clone1);
-		var clone2 = $('.menu-2 > ul').clone();
-		$('#fh5co-offcanvas').append(clone2);
+		// $('#page').prepend('<div id="fh5co-offcanvas" />');
+		// $('#page').prepend('<a href="#" class="js-fh5co-nav-toggle fh5co-nav-toggle fh5co-nav-white"><i></i></a>');
+		// var clone1 = $('.menu-1 > ul').clone();
+		// $('#fh5co-offcanvas').append(clone1);
+		// var clone2 = $('.menu-2 > ul').clone();
+		// $('#fh5co-offcanvas').append(clone2);
 
-		$('#fh5co-offcanvas .has-dropdown').addClass('offcanvas-has-dropdown');
-		$('#fh5co-offcanvas')
-			.find('li')
-			.removeClass('has-dropdown');
+		// $('#fh5co-offcanvas .has-dropdown').addClass('offcanvas-has-dropdown');
+		// $('#fh5co-offcanvas')
+		// 	.find('li')
+		// 	.removeClass('has-dropdown');
 
-		// Hover dropdown menu on mobile
-		$('.offcanvas-has-dropdown').mouseenter(function(){
-			var $this = $(this);
+		// // Hover dropdown menu on mobile
+		// $('.offcanvas-has-dropdown').mouseenter(function(){
+		// 	var $this = $(this);
 
-			$this
-				.addClass('active')
-				.find('ul')
-				.slideDown(500, 'easeOutExpo');				
-		}).mouseleave(function(){
+		// 	$this
+		// 		.addClass('active')
+		// 		.find('ul')
+		// 		.slideDown(500, 'easeOutExpo');				
+		// }).mouseleave(function(){
 
-			var $this = $(this);
-			$this
-				.removeClass('active')
-				.find('ul')
-				.slideUp(500, 'easeOutExpo');				
-		});
+		// 	var $this = $(this);
+		// 	$this
+		// 		.removeClass('active')
+		// 		.find('ul')
+		// 		.slideUp(500, 'easeOutExpo');				
+		// });
 
 
-		$(window).resize(function(){
+		// $(window).resize(function(){
 
-			if ( $('body').hasClass('offcanvas') ) {
+		// 	if ( $('body').hasClass('offcanvas') ) {
 
-    			$('body').removeClass('offcanvas');
-    			$('.js-fh5co-nav-toggle').removeClass('active');
+    	// 		$('body').removeClass('offcanvas');
+    	// 		$('.js-fh5co-nav-toggle').removeClass('active');
 				
-	    	}
-		});
+	    // 	}
+		// });
 	};
 
 
